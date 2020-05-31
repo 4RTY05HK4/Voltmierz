6ac2545d8cb348c64f64e4c0313e3c0b151c2c81
4RT3M <33829671+4RTY05HK4@users.noreply.github.com>
2020-05-31 17:30:50 +0200

    possibly final commit
    better measurement resolution, code cleaned and commented

--- Woltomierz/LCD_lib.h
+++ Woltomierz/LCD_lib.h
@@ -1,7 +1,29 @@
+/*
+	Created: 30.05.2020 13:05:06
+	Author : Jakub Nowak
+	Biblioteka do wyœwietlania napiêcia zmierzonego za pomoc¹ wbudowanego w mikrokontroler Atmega32 przetwornika AC.
+	-------------------------------------------------------!!!UWAGA!!!----------------------------------------------------
+	|                             MAKSYMALNE NAPIÊCIE JAKIE MO¯E ZOSTAC ZMIERZONE TO 2.55V                               |
+	----------------------------------------------------------------------------------------------------------------------
+	Wyœwietlacz nale¿y pod³¹czyæ do kontrolera poprzez interfejs I2C. Porty SCL i SDA nale¿y pod³¹czyæ na porty kontrolera PC0 i PC1.
+	SCL => PC0
+	SDA => PC1
+	Do wyjœæ analogowych przetwornika AC nale¿y pod³¹czyæ potencjometry. U¿yte wejœcia to PA1 i PA2.
+	Po odpowiednim pod³¹czeniu mo¿na w g³ównym programie wywo³aæ funkcje demo(). Wówczas odczyt z pierwszego kana³u wyœwietlony zostanie na górnej linii
+	wyœwietlacza z dok³adnoœci¹ 0.16V, a odczyt z drugiego kana³u wyœwietlony zostanie na drugiej linii wyœwietlacza z dok³adnoœci¹ 0.03V;
+
+*/
+
 #define RS 0
 #define RW 1
 #define E 2
 #define LED 3
+
+uint8_t bar_1[]={0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10}; //znak w³asny pojedyñczej belki
+uint8_t bar_2[]={0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18}; //znak w³asny podwójnej belki
+uint8_t bar_3[]={0x1c,0x1c,0x1c,0x1c,0x1c,0x1c,0x1c,0x1c}; //znak w³asny potrójnej belki
+uint8_t bar_4[]={0x1e,0x1e,0x1e,0x1e,0x1e,0x1e,0x1e,0x1e}; //znak w³asny poczwórnej belki
+uint8_t bar_5[]={0x1f,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f}; //znak w³asny pe³nego prostok¹ta
 
 void TWI_init()
 {
@@ -46,6 +68,15 @@ void LCD_write(uint8_t data, uint8_t rs)
 
 }
 
+void LCD_bars(uint8_t adres, uint8_t *tab)
+{
+	for (uint8_t i=0;i<8;i++)
+	{
+		LCD_write(0x40+adres*8+i,0);
+		LCD_write(tab[i],1);
+	}
+}
+
 void LCD_init()
 {
 	TWI_start();
@@ -55,12 +86,18 @@ void LCD_init()
 	LCD_write(0x28, 0);
 	LCD_write(0x06, 0);
 	LCD_write(0x0c, 0);
+	//dodanie w³asnych znaków przed czyszczeniem ekranu
+	LCD_bars(1,bar_1);
+	LCD_bars(2,bar_2);
+	LCD_bars(3,bar_3);
+	LCD_bars(4,bar_4);
+	LCD_bars(5,bar_5);
 	LCD_write(0x01, 0);
 	TWI_stop();
 	_delay_ms(2);
 }
 
-void LCD_text(char *napis, uint8_t adres)
+void LCD_text(char *napis, uint8_t adres) 
 {
 	uint8_t i=0;
 	TWI_start();
@@ -68,24 +105,112 @@ void LCD_text(char *napis, uint8_t adres)
 	LCD_write(adres, 0);
 	while(napis[i]!='\0')
 	{
+		if (napis[i]=='1') napis[i]=1;
+		if (napis[i]=='2') napis[i]=2;
+		if (napis[i]=='3') napis[i]=3;
+		if (napis[i]=='4') napis[i]=4;
+		if (napis[i]=='5') napis[i]=5;
 		LCD_write(napis[i++], 1);
 		
 	}
 	TWI_stop();
 }
 
+void ADC_init(void)
+{
+	ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<MUX0);
+	ADCSRA = (1<<ADEN)|(1<<ADPS2);
+}
 
+uint16_t ADC_CH_1() //funkcja mierz¹ca napiêcie na wejœciu pierwszym (PA1), zwracaj¹ca wartoœæ napiêcia do zmiennej
+{
+	ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<MUX0)|(0<<MUX1);
+	ADCSRA |= (1<<ADSC);
+	while (ADCSRA & (1<<ADSC));
+	uint16_t wynik = ADC;
+	wynik = (wynik>>2);
+	return wynik;
+}
 
-//void TWI_init();
-//
-//void TWI_start();
-//
-//void TWI_stop(void);
-//
-//void TWI_write(uint8_t bajt);
-//
-//void LCD_write(uint8_t data, uint8_t rs);
-//
-//void LCD_init();
-//
-//void LCD_text(char *napis, uint8_t adres);
+uint16_t ADC_CH_2() //funkcja mierz¹ca napiêcie na wejœciu drugim (PA2), zwracaj¹ca wartoœæ napiêcia do zmiennej
+{
+	ADMUX = (1<<REFS1)|(1<<REFS0)|(0<<MUX0)|(1<<MUX1);
+	ADCSRA |= (1<<ADSC);
+	while (ADCSRA & (1<<ADSC));
+	uint16_t wynik = ADC;
+	wynik = (wynik>>2);
+	return wynik;
+}
+
+void LCD_V_disp(uint16_t wynik, uint8_t adres) //funkcja wyœwietlaj¹ca prostok¹t na ka¿de 0.16V, w programie g³ównym nale¿y jej przekazaæ zmierzone napiêcie oraz adres linii wyœwietlacza
+{
+	uint8_t wart=0;
+	wart = wynik / 16;
+	if(wart == 0) 
+	{
+	LCD_text(" ",adres);
+	}
+	else
+	{
+		for(uint8_t i=0; i<=wart; i++)
+		{
+			LCD_text("5",adres);
+			adres++;
+		}
+	}
+	for(uint8_t i=wart; i<=16; i++)
+	{
+		LCD_text(" ",adres);
+		adres++;
+	}
+}
+
+void LCD_V_prec_disp(uint16_t wynik, uint8_t adres) //funkcja wyœwietlaj¹ca belkê na ka¿de 0.03V, , w programie g³ównym nale¿y jej przekazaæ zmierzone napiêcie oraz adres linii wyœwietlacza
+{
+	uint8_t wart = 0;
+	wart = wynik/3;
+	uint8_t iter = wart/5;	
+	uint8_t rest = wart%5;
+
+	if(wart == 0)
+	{
+		LCD_text(" ",adres);
+	}
+	else
+	{
+		if(rest == 1 && iter <= 1) LCD_text("1",adres);
+		else if(rest == 2 && iter <= 1) LCD_text("2",adres);
+		else if(rest == 3 && iter <= 1) LCD_text("3",adres);
+		else if(rest == 4 && iter <= 1) LCD_text("4",adres);
+		for(uint8_t i = 1; i <= iter; i++)
+		{
+			LCD_text("5",adres);
+			adres++;
+		}
+	}
+	if(rest == 1) LCD_text("1",adres);
+	else if(rest == 2) LCD_text("2",adres);
+	else if(rest == 3) LCD_text("3",adres);
+	else if(rest == 4) LCD_text("4",adres);
+	adres++;
+	for(uint8_t i=iter; i<=16; i++)
+	{
+		LCD_text(" ",adres);
+		adres++;
+	}
+}
+
+void demo()
+{
+    TWI_init(); 
+    LCD_init(); 
+    uint16_t wynik;
+    ADC_init(); 
+    while (1)
+    {
+	    wynik = ADC_CH_1();
+	    LCD_V_disp(wynik,0x80); 
+	    wynik = ADC_CH_2(); 
+	    LCD_V_prec_disp(wynik,0xc0); 
+    }
+}
--- Woltomierz/main.c
+++ Woltomierz/main.c
@@ -2,92 +2,29 @@
  * Woltomierz.c
  *
  * Created: 30.05.2020 13:05:06
- * Author : Jakub
+ * Author : Jakub Nowak
  */ 
-
 
 #include <avr/io.h>
 #define F_CPU 16000000
 #include <util/delay.h>
 #include "LCD_lib.h"
 
-void ADC_init(void)
-{
-	ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<MUX0);
-	ADCSRA = (1<<ADEN)|(1<<ADPS2);
-}
-
-void BCD_ADC(uint16_t wynik, uint8_t *tab)
-{
-	tab[0] = wynik / 100;
-	tab[1] = (wynik % 100) / 10;
-	tab[2] = (wynik % 100) % 10;
-}
-
-void LCD_V_disp(uint16_t wynik, uint8_t adres)
-{
-	uint8_t wart=0;
-	wart = wynik / 16;
-	for(uint8_t i=0; i<=wart; i++)
-	{
-		LCD_text("0",adres);
-		adres++;
-	}
-	for(uint8_t i=wart; i<=16; i++)
-	{
-		LCD_text(".",adres);
-		adres++;
-	}
-}
-
 int main(void)
 {
-    TWI_init();
-    LCD_init();
+    TWI_init(); // wywo³anie funkcji inicjalizuj¹cych interfejs I2C
+    LCD_init(); // wywo³anie funkcji inicjalizuj¹cych wyœwietlacz LCD za poœrednictwem interfejsu I2C
 	uint16_t wynik;
-	uint8_t LCD[3];
-	char value[] = "1,23V";
-	ADC_init();
-	//ADCSRA |= (1<<ADSC);
-	//while (ADCSRA & (1<<ADSC));
-	//wynik = ADC;
-	//wynik = (wynik>>2);
-	//BCD_ADC(wynik, LCD);
-	//value[0] = LCD[0]+'0';
-	//value[2] = LCD[1]+'0';
-	//value[3] = LCD[2]+'0';
-	//LCD_text(value,0x80);
-	//////uint8_t adres = 0x80;
-	//////for(uint8_t i=0; i<6; i++)
-	//////{
-		//////LCD_text("0",adres);
-		//////adres++;
-	//////}
+	ADC_init(); // inicjalizacja przetwornika AC
 
     while (1) 
     {	
-		ADMUX = (1<<REFS1)|(1<<REFS0)|(1<<MUX0);
-		ADCSRA |= (1<<ADSC);
-		while (ADCSRA & (1<<ADSC));
-		wynik = ADC;
-		wynik = (wynik>>2);
-		//BCD_ADC(wynik, LCD);
-		//value[0] = LCD[0]+'0';
-		//value[2] = LCD[1]+'0';
-		//value[3] = LCD[2]+'0';
-		//LCD_text(value,0x80);
-		LCD_V_disp(wynik,0x80);
-		ADMUX = (1<<REFS1)|(1<<REFS0)|(0<<MUX0)|(1<<MUX1);
-		ADCSRA |= (1<<ADSC);
-		while (ADCSRA & (1<<ADSC));
-		wynik = ADC;
-		wynik = (wynik>>2);
-		//BCD_ADC(wynik, LCD);
-		//value[0] = LCD[0]+'0';
-		//value[2] = LCD[1]+'0';
-		//value[3] = LCD[2]+'0';
-		//LCD_text(value,0xc0);
-		LCD_V_disp(wynik,0xc0);
+		wynik = ADC_CH_1(); // zwrócenie zmierzonej wartoœci na kanale PA1 do zmiennej wynik
+		LCD_V_disp(wynik,0x80); // wyœwietlenie napiêcia z mniejsz¹ podzia³k¹ (16 stanów) na wyœwietlaczu LCD
+
+		wynik = ADC_CH_2(); // zwrócenie zmierzonej wartoœci na kanale PA2 do zmiennej wynik
+		LCD_V_prec_disp(wynik,0xc0); // wyœwietlenie napiêcia z wiêksz¹ podzia³k¹ (80 stanów) na wyœwietlaczu LCD
+		
     }
 }
 
